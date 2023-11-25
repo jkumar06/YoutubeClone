a40017420227fc055eff0b5b34b4c84eb633f7c1
jagadeesh kumar <jagadeesh.kumar@bolt.earth>
2023-11-25 16:12:01 +0530

    changes in code

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,10 +1,44 @@
-import React from 'react'
-import { useDispatch } from 'react-redux'
+import React,{useState,useEffect} from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { toggleMenu } from '../utils/appSlice';
+import { YOUTUBE_SEARCH_API } from '../utils/constants';
+import { cacheResults } from '../utils/searchSlice';
 
 const Head = () => {
-    const dispatch = useDispatch();
+    const [searchQuery,setSearchQuery] = useState('');
+    const [suggestions,setSuggestions] = useState([]);
+    const [showSuggestions,setShowSuggestions] = useState(false);
 
+    const searchCache = useSelector(store => store.search);
+
+    useEffect(() => {
+     
+     const timer = setTimeout(() => {
+        if(searchCache[searchQuery]) {
+            setSuggestions(searchCache[searchQuery])
+        } else {
+            getSearchSuggestions()
+        }
+    },200);
+     
+     return () => {
+        clearTimeout(timer)
+     }
+    },[searchQuery]);
+
+    const getSearchSuggestions = async () => {
+        console.log("API CALL",searchQuery)
+        const data = await fetch(YOUTUBE_SEARCH_API + searchQuery);
+        const json = await data.json();
+        setSuggestions(json[1]);
+
+        dispatch(
+            cacheResults({
+            [searchQuery]: json[1]
+        }))
+    }
+
+    const dispatch = useDispatch();
     const toggleMenuHandler = () => {
         dispatch(toggleMenu());
     }
@@ -29,13 +63,26 @@ const Head = () => {
         </div>
        
         <div className='col-span-10 px-10'>
+            <div>
             <input
-                className='w-1/2 border border-grey-400 p-2 rounded-l-full'
-                type='text'/>
+                className=' px-5 w-1/2 border border-grey-400 p-2 rounded-l-full'
+                value={searchQuery}
+                type='text'
+                onChange={(e) => setSearchQuery(e.target.value)}
+                onFocus={() => setShowSuggestions(true)}
+                onBlur={() => setShowSuggestions(false)}
+                />
             <button 
                 className='border border-gray-400 px-5 p-2 rounded-r-full gb-grey-100'>
                 🔍
                 </button>
+                </div>
+           { showSuggestions && ( <div className='fixed bg-white py-2 px-2 w-[34rem] shadow-lg rounded-lg border border-gray-100'>
+                <ul>
+                    {suggestions.map((s) =><li className='py-2 px-3 shadow-sm hover:bg-gray-100'>🔍 {s}</li>)}
+                </ul>
+            </div>)}
+      
         </div>
         <div className='col-span-1'> 
             <img   
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,3 +3,8 @@ const GOOGLE_API_KEY = "AIzaSyBZAZJNtBVPxF_B587equGtwZwbp-dVQHg";
 export const YOUTUBE_VIDEO_API = 
     "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" + GOOGLE_API_KEY;
 
+// export const YOUTUBE_SEARCH_API = 
+//     "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+
+export const YOUTUBE_SEARCH_API =
+  "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
